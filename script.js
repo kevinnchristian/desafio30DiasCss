314782fbc50067d1fad93ac846ae9e8f917fe2c9
kevinnchristian <kevin.cmribeiro@outlook.com>
2022-02-04 13:34:03 -0300

    :rocket: card implementation with 3d effect

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const header = document.querySelector('header');
+const header = document.querySelector("header");
 
-window.addEventListener('scroll', function() {
+window.addEventListener("scroll", function () {
   if (scrollY > 80) {
-    header.classList.add('backgroundHeader');
+    header.classList.add("backgroundHeader");
   } else {
-    header.classList.remove('backgroundHeader');
+    header.classList.remove("backgroundHeader");
   }
-})
+});
 
-
+VanillaTilt.init(document.querySelectorAll(".main-card"), {
+  scale: 1.1,
+  glare: true,
+  reverse: true,
+  "max-glare": 0.5,
+});
